fe0b1ffc9e35ca651a8f364d7897fcf81a9fb782
Mike Ziebeck <mike@ziebeck.net>
2023-06-06 23:00:37 +0200

    fix: Test Message2

--- server/io.js
+++ server/io.js
@@ -19,6 +19,22 @@ export default function Svc(socket, io) {
                 }, 1000);
             });
         },
+        getMessage2(data) {
+            return new Promise((resolve) => {
+                const msgs = ["Hi, this is a chat message from IO server!", "Hi, this is another chat message from IO server!"];
+                let msgIdx = 0;
+                socket.emit("chatMessage4", { data: "Hi again" });
+                socket.emit("chatMessage5", { data: "Hi again from 5" });
+                const timer = setInterval(() => {
+                    socket.emit("chatMessage2", msgs[msgIdx]);
+                    socket.emit("chatMessage3", "sending chat message3...");
+                    if (++msgIdx >= msgs.length) {
+                        clearInterval(timer);
+                        resolve("It worked! Received msg: " + JSON.stringify(data));
+                    }
+                }, 500);
+            });
+        },
         bidirectional(data) {
             console.log("bidirectional rxd!", data);
             return new Promise((resolve) => {
